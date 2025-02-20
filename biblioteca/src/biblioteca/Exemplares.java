eab62187e6c99e9f484a93e364d9519dbd611763
iuri4960 <iuriferbastos@gmail.com>
2025-02-20 19:59:17 -0300

    Add files via upload

---
+++ biblioteca/src/biblioteca/Exemplares.java
@@ -0,0 +1,32 @@
+package biblioteca;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Exemplares {
+	private Map<String, List<Livro>> catalogo;
+	
+	public Exemplares() {
+		this.catalogo= new HashMap<>();
+	}
+	
+	public void colocarLivro(Livro livro) {
+		catalogo.putIfAbsent(livro.getNome(), new ArrayList<>());
+        catalogo.get(livro.getNome()).add(livro);
+	}
+	
+	public List<Livro> obterExemplares(String nomeLivro) {
+		return catalogo.getOrDefault(nomeLivro, Collections.emptyList());
+	}
+	public void exibirCatalogo() {
+	    for (Map.Entry<String, List<Livro>> entry : catalogo.entrySet()) {
+	        System.out.println("Nome: " + entry.getKey() + " | Exemplares: " + entry.getValue().size());
+	            for (Livro livro : entry.getValue()) {
+	                System.out.println("  - " + livro);
+	      }
+	   }
+	}
+}
---
+++ biblioteca/src/biblioteca/GerenciarLivro.java
@@ -0,0 +1,60 @@
+package biblioteca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import excepetion.LivroJaAdicionadoException;
+import excepetion.LivroNaoEncontradoException;
+
+public class GerenciarLivro implements SistemaBibliotecarioCollection {
+	List<Livro> livros = new ArrayList<Livro>();
+
+	//Método para adicionar livros
+	public void adicionarLivro(Livro livro) throws LivroJaAdicionadoException {
+		try {
+			Livro l= consultarLivro(livro.getCodigo());
+			throw new LivroJaAdicionadoException("Livro já adicionado");
+		} catch (LivroNaoEncontradoException e){
+			livros.add(livro);
+		}	
+	}
+
+	//Método para procurar um livro
+	public Livro consultarLivro(String codigo) throws LivroNaoEncontradoException {
+		Livro l= null;
+		for (Livro livro: livros) {
+			if (livro.getCodigo().equalsIgnoreCase(codigo)) {
+				l=livro;
+				break;
+			}
+		}
+		if (l==null) { throw new LivroNaoEncontradoException("Livro não encontrado"); }
+		return l;
+	}
+
+	//Método para remover um livro
+	public void removerLivro(String codigo) throws LivroNaoEncontradoException {
+		Livro li= consultarLivro(codigo);
+		if (li!= null) {
+			livros.remove(li);
+		}
+		
+	}
+
+	//Método para mostrar todos os livros
+	public Livro[] listaLivros() {
+		Livro[] retorno = new Livro[livros.size()];
+		return livros.toArray(retorno);
+	}
+
+	//Método para atualizar as informações de um livro
+	public void atualizarInformacao(String codigo, Livro novoLivro) throws LivroNaoEncontradoException {
+		Livro l= this.consultarLivro(codigo);
+		if (l!=null) {
+			int pos= livros.indexOf(l);
+			livros.set(pos, novoLivro);
+		}
+		
+	}
+
+}
