34464d4cda4c406979ebb7e39609613062d81c94
deleanestev <delean.estevao@hotmail.com>
2025-02-24 10:49:43 -0300

    Update SistemaBibliotecarioCollection.java
    Seguir a implementação do SistemaBibliotecario (implementação da Interface)

--- biblioteca/src/biblioteca/SistemaBibliotecarioCollection.java
+++ biblioteca/src/biblioteca/SistemaBibliotecarioCollection.java
@@ -1,8 +1,15 @@
 package biblioteca;
 
+import java.util.ArrayList;
+
+import excepetion.EmprestimoJaAdicionadoException;
+import excepetion.EmprestimoNaoEncontradoException;
 import excepetion.LivroJaAdicionadoException;
 import excepetion.LivroNaoEncontradoException;
+import excepetion.MultaNaoPagaException;
 import excepetion.NumeroPaginasInvalidoException;
+import excepetion.UsuarioJaAdicionadoException;
+import excepetion.UsuarioNaoEncontradoException;
 
 public interface SistemaBibliotecarioCollection {
 	public void adicionarLivro(Livro livro) throws LivroJaAdicionadoException, NumeroPaginasInvalidoException;
@@ -11,16 +18,16 @@ public interface SistemaBibliotecarioCollection {
 	public void atualizarInformacao(String codigo, Livro novoLivro) throws LivroNaoEncontradoException;
 	public Livro[] listaLivros();
 	
-	public void adicionarEmprestimo();
-	public Emprestimo consultarEmprestimo();
-	public void editarEmprestimo();
-	public void removerEmprestimo();
-	public void atualizarMulta();
-	public void removerMulta();
+	public void adicionarEmprestimo(Emprestimo emprestimo) throws EmprestimoJaAdicionadoException;
+	public Emprestimo consultarEmprestimo(String codigo) throws LivroNaoEncontradoException, EmprestimoNaoEncontradoException;
+	public ArrayList<Emprestimo> consultarEmprestimo(int matricula) throws UsuarioNaoEncontradoException, EmprestimoNaoEncontradoException;
+	public void editarEmprestimo(Livro livro, Emprestimo emprestimo) throws EmprestimoNaoEncontradoException;
+	public void removerEmprestimo(Livro livro) throws EmprestimoNaoEncontradoException, MultaNaoPagaException;
 	
-	public void adicionarUsuario();
-	public Usuario consultarUsuario();
-	public void editarUsuario();
-	public void removerUsuario();
+	public void adicionarUsuario(Usuario usuario) throws UsuarioJaAdicionadoException;
+	public Usuario consultarUsuario(int matricula) throws UsuarioNaoEncontradoException;
+	public void editarUsuario(int matricula, Usuario usuario) throws UsuarioNaoEncontradoException;
+	public void removerUsuario(int matricula) throws UsuarioNaoEncontradoException;
 	
 }
+
