0e0ca5ef1a3fb47d7bd76c5a12d8178bbb8f64ff
iuri4960 <iuriferbastos@gmail.com>
2025-02-25 21:51:40 -0300

    Update GerenciarLivro.java

--- biblioteca/src/biblioteca/GerenciarLivro.java
+++ biblioteca/src/biblioteca/GerenciarLivro.java
@@ -6,9 +6,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import excepetion.LivroJaAdicionadoException;
-import excepetion.LivroNaoEncontradoException;
-import excepetion.NumeroPaginasInvalidoException;
+import exception.LivroJaAdicionadoException;
+import exception.LivroNaoEncontradoException;
+import exception.NumeroPaginasInvalidoException;
 
 public class GerenciarLivro implements Serializable {
 	List<Livro> livros = new ArrayList<Livro>();
